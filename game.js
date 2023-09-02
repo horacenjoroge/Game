1e9ad95cb7fa708abfbb5a50430fd218cf8d57f0
hozzy <125595726+horacenjoroge@users.noreply.github.com>
2023-09-02 08:21:52 +0300

    change functionality

--- game.js
+++ game.js
@@ -8,4 +8,24 @@ const randomWord = getComputerChoice();
 console.log (randomWord);
 function playRound(playerSelection,computerselection){
     const playerSelection =prompt("choose rock,paper or scissors").toLowerCase();
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        console.log("Computer chose: " + computerSelection);
+        console.log("You chose: " + playerSelection);
+
+        if (playerSelection === computerSelection) {
+            console.log("It's a tie!");
+        } else if (
+            (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")
+        ) {
+            console.log("You win!");
+        } else {
+            console.log("Computer wins!");
+        }
+    } else {
+        console.log("Invalid input. Please choose rock, paper, or scissors.");
+    }
 }
+
+playRound();
