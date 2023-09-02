1da6c1d3b47ad566d4d78324cb4a11072f5dadaa
hozzy <125595726+horacenjoroge@users.noreply.github.com>
2023-09-02 07:55:28 +0300

    function playRound

--- game.js
+++ game.js
@@ -6,3 +6,19 @@ return words[randomIndex];
 }
 const randomWord = getComputerChoice();
 console.log (randomWord);
+function playRound(playerSelection,computerselection){
+    const playerSelection =prompt("choose rock,paper or scissors").toLowerCase();
+    if (userInput === "rock") {
+        // Handle user choosing rock
+        console.log("You chose rock.");
+    } else if (userInput === "paper") {
+        // Handle user choosing paper
+        console.log("You chose paper.");
+    } else if (userInput === "scissors") {
+        // Handle user choosing scissors
+        console.log("You chose scissors.");
+    } else {
+        // Handle invalid input
+        console.log("Invalid input. Please choose rock, paper, or scissors.");
+    }
+}
